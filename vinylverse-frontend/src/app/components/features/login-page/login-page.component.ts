c21403c3fe24399db8c6366ae1826c8815604915
Marko Jeremic <markojeremic03@gmail.com>
2026-03-14 13:19:58 +0100

    dodan rate limiting za login

--- vinylverse-frontend/src/app/components/features/login-page/login-page.component.ts
+++ vinylverse-frontend/src/app/components/features/login-page/login-page.component.ts
@@ -16,14 +16,51 @@ export class LoginPageComponent {
   email = '';
   lozinka = '';
   errorMessage = '';
+  countdown = 0;
+  private countdownInterval: any;
 
   constructor(private loginService: LoginService, private router: Router) {}
 
+  startCountdown(seconds: number) {
+    this.countdown = seconds;
+    if (this.countdownInterval) {
+      clearInterval(this.countdownInterval);
+    }
+    this.countdownInterval = setInterval(() => {
+      this.countdown--;
+      if (this.countdown <= 0) {
+        clearInterval(this.countdownInterval);
+        this.countdownInterval = null;
+        this.errorMessage = '';
+      }
+    }, 1000);
+  }
+
   login() {
+    if (this.countdown > 0) {
+      return;
+    }
     const user = { email: this.email, lozinka: this.lozinka };
     this.loginService.login(user).subscribe({
-      next: () => this.router.navigate(['/']),
-      error: () => this.errorMessage = 'Neuspešan login. Proveri podatke.'
+      next: () => {
+        this.errorMessage = '';
+        this.countdown = 0;
+        if (this.countdownInterval) {
+          clearInterval(this.countdownInterval);
+          this.countdownInterval = null;
+        }
+        this.router.navigate(['/']);
+      },
+      error: (error: any) => {
+        if (error.status === 401) {
+          this.errorMessage = 'Neuspešan login. Proveri podatke.';
+        } else if (error.status === 429) {
+          this.errorMessage = 'Previše pokušaja. Pokušaj ponovo kasnije.';
+          this.startCountdown(60);
+        } else {
+          this.errorMessage = 'Došlo je do greške. Pokušaj ponovo kasnije.';
+        }
+      },
     });
   }
 }
